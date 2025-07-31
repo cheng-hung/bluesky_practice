8b2b7330b8550f1b7aa694cf9ea2ae38bcab6a68
Cheng-Hung Lin <clin1@bnl.gov>
2025-07-31 01:13:16 -0400

    work on pilatus kafka 2.1

--- PDF_plan/kafka/pilatus_sum_v2.1.py
+++ PDF_plan/kafka/pilatus_sum_v2.1.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import yaml, tifffile
 # %matplotlib widget
 from configparser import ConfigParser
+import pandas as pd
 
 from tiled.client import from_profile
 tiled_client = from_profile('pdf')
@@ -39,6 +40,15 @@ def file_prefix_producer(uid, tiled_client):
 
 
 
+def iq_saver(fn, df, md, header=['q_A^-1', 'I(q)']):
+    with open(fn, mode='w', encoding='utf-8') as f:
+        f.write('pyFai_poni_information_28ID1_NSLS2_BNL\n')
+        for key, value in md.items():
+            f.write(f'{key} {value}\n')
+    # Now append the dataframe without a header
+    df.to_csv(fn, encoding='utf-8', mode='a', header=header, index=False, float_format='{:.8e}'.format, sep=' ')
+
+
 
 class Pilatus_config(ConfigParser):
     """The configuration for the server."""
@@ -55,8 +65,7 @@ class Pilatus_config(ConfigParser):
 
 
 
-
-class Pilatus_pro(Pilatus_config):
+class Pilatus_sum(Pilatus_config):
 
     def __init__(self, uid, tiled_client, config_fn, **kwargs):
         self.uid = uid
@@ -128,43 +137,55 @@ class Pilatus_pro(Pilatus_config):
         n = self.get('PATH', 'flat_field_fn', fallback='flat_filed.tiff')
         return os.path.join(self.config_base, n_folder, n)
     
+    @property
+    def temperature(self):
+        try:
+            temp_controller = self.get('TEMPERATURE', 'temp_controller', fallback='cryostat_A')
+            T = self.run.start['more_info'][temp_controller]
+
+        except (KeyError, IndexError, TypeError):
+            T = 'None'
+
+        return T
+
 
     @property
-    def binning(self):
-        return self.getint('INTEGRATION', 'binning', fallback=4096)
+    def readable_time(self):
+        t = _readable_time(self.run.start['time'])
+        return f'{t[0]}-{t[1]}'
     
     @property
-    def polarization(self):
-        return self.getfloat('INTEGRATION', 'polarization', fallback=0.99)
-    
-    @property
-    def UNIT(self):
-        return self.get('INTEGRATION', 'UNIT', fallback="q_A^-1")
-    
-    @property
-    def ll(self):
-        return self.getfloat('INTEGRATION', 'low_limit_pcfilter', fallback=1.0)
-    
-    @property
-    def ul(self):
-        return self.getfloat('INTEGRATION', 'up_limit_pcfilter', fallback=99.0) 
+    def file_name_prefix(self):
+        T = self.temperature
 
-
-
-    def is_pdf_xrd(self, distance_limit=0.3):
-        ## unit of distance is meter
-
-        # run = tiled_client[uid]
-        distance = self.run.start['calibration_md']['Distance']
-        acq_mode = ''
-
-        if distance < distance_limit:
-            acq_mode = 'PDF'
-
-        elif distance > distance_limit:
-            acq_mode = 'XRD'
+        if type(T) is float:
+            return f'{self.sample_name}_{self.readable_time}_{self.full_uid:6.6}_{T}_K'
 
         else:
+            return f'{self.sample_name}_{self.readable_time}_{self.full_uid:6.6}'
+        
+    
+
+    def is_pdf_xrd(self, distance_limit=0.3, ):
+        ## unit of distance is meter
+        # run = tiled_client[uid]
+
+        try:
+            distance = self.run.start['calibration_md']['Distance']
+            acq_mode = ''
+
+            if distance < distance_limit:
+                acq_mode = 'PDF'
+
+            elif distance > distance_limit:
+                acq_mode = 'XRD'
+
+            else:
+                acq_mode = 'PDF'
+
+        except KeyError:
+            print('\nCannot find distance in metadata in start doc')
+            print('\nUse PDF as default mode.')
             acq_mode = 'PDF'
 
         return acq_mode
@@ -184,16 +205,11 @@ class Pilatus_pro(Pilatus_config):
             my_im2 = my_im2 / flat_field
             my_im1 = my_im1 / flat_field
 
-
         if self.is_pdf_xrd() == 'PDF':
             mask_dir = self.pilatus_PDF
 
-        elif self.is_pdf_xrd() == 'XRD':
-            mask_dir = self.pilatus_XRD
-
         else:
-            mask_dir = self.pilatus_PDF
-
+            mask_dir = self.pilatus_XRD
 
         # masks_pos_fn = ['Mask_pos1_ext_BS.npy', 'Mask_pos2_ext_BS.npy', 'Mask_pos3_ext_BS.npy']
         m1_path = os.path.join(mask_dir, self.masks_pos_flist[0])
@@ -218,234 +234,132 @@ class Pilatus_pro(Pilatus_config):
 
 
 
-
     def save_image_sum_T(self):
 
         data_dir = os.path.join(self.tiff_base, self.sample_name, "dark_sub")
 
-        File_Name_Prefix = file_prefix_producer(self.uid, self.tiled_client)
         sum_dir = os.path.join(data_dir, 'sum')
-        os.makedirs(sum_dir, exist_ok=True)  # Create 'sum' directory if it doesn't exist
-            
-        try:
-            temp_controller = self.get('TEMPERATURE', 'temp_controller', fallback='cryostat_A')
-            T = self.run.start['more_info'][temp_controller]
-
-        except (KeyError, IndexError, TypeError):
-            T = 'None'
+        os.makedirs(sum_dir, exist_ok=True)  # Create 'sum' directory if it doesn't exis
         
         full_imsum = self.sum_everything()
         
         # Save output file to the new 'sum' directory
-        if type(T) is float:
-            sum_tiff_fn = os.path.join(sum_dir, f'{File_Name_Prefix}_{T}_K_sum.tiff')
-        else:
-            sum_tiff_fn = os.path.join(sum_dir, f'{File_Name_Prefix}_sum.tiff')
-        
-        
+        sum_tiff_fn = os.path.join(sum_dir, f'{self.file_name_prefix}_sum.tiff')
         tifffile.imsave(sum_tiff_fn, full_imsum)
         print(f'\n*** {os.path.basename(sum_tiff_fn)} saved!! ***\n')
 
-        saved_fn_prefix = os.path.basename(sum_tiff_fn).split('.')[0]
-
-        return full_imsum, sum_dir, saved_fn_prefix
+        return full_imsum, sum_dir
 
 
 
-    def pct_integration(self, img, fn_prefix):
-    # def pct_integration(img, fn_prefix, binning=5000, polarization=0.99, UNIT = "q_A^-1", ll=1, ul=99, 
-    #                     poni_fn = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/config_base/merged_PDF/merged.poni', 
-    #                     mask_fn = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/config_base/pilatus_mask/stitched_2.npy',
-    #                     directory = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/tiff_base',
-    #                     ):
+
+
+class Pilatus_Int(Pilatus_sum):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def merged_poin(self):
+        n = self.get('PATH', 'merged_poin', fallback='merged.poni')
+
+        if self.is_pdf_xrd() == 'PDF':
+            n_folder = self.pilatus_PDF
+        else:
+            n_folder = self.pilatus_XRD
+
+        return os.path.join(n_folder, n)
+    
+
+    @property
+    def stitched_mask(self):
+        n = self.get('PATH', 'stitched_mask', fallback='stitched_mask.npy')
+
+        if self.is_pdf_xrd() == 'PDF':
+            n_folder = self.pilatus_PDF
+        else:
+            n_folder = self.pilatus_XRD
+
+        return os.path.join(n_folder, n)
+
+
+    @property
+    def binning(self):
+        return self.getint('INTEGRATION', 'binning', fallback=4096)
+    
+    @property
+    def polarization(self):
+        return self.getfloat('INTEGRATION', 'polarization', fallback=0.99)
+    
+    @property
+    def UNIT(self):
+        return self.get('INTEGRATION', 'UNIT', fallback="q_A^-1")
+    
+    @property
+    def ll(self):
+        return self.getfloat('INTEGRATION', 'low_limit_pcfilter', fallback=1.0)
+    
+    @property
+    def ul(self):
+        return self.getfloat('INTEGRATION', 'up_limit_pcfilter', fallback=99.0) 
+
+    
+    def pct_integration(self, full_imsum, sum_dir):
+
+        ai = pyFAI.load(self.merged_poin)
+        mask0 = np.load(self.stitched_mask)
         
-        ai = pyFAI.load(poni_fn)
-        mask0 = np.load(mask_fn)
+        ## perform azimuthalintegration on one image to retain 2D information
+        ## i2d.shape is (npt_azim, self.binning) which corresponds the intensity of 2D image cake
+        ## q2d.shape is (self.binning, )
+        i2d, q1d, chi1d = ai.integrate2d(full_imsum, self.binning, 
+                                         unit=self.UNIT, npt_azim=3600, 
+                                         polarization_factor=self.polarization, 
+                                         mask=mask0) 
         
-        i2d, q2d, chi2d = ai.integrate2d(img, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0) # perform azimuthalintegration on one image to retain 2D information
-        intrinsic_mask_unrolled,_,_ = ai.integrate2d(mask0, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0)  #trasnform mask0 (base mask) to the same coordinate space and cast it as type bool
+        ## trasnform mask0 (base mask) to the same coordinate space and cast it as type bool
+        intrinsic_mask_unrolled, _, _ = ai.integrate2d(mask0, self.binning, 
+                                                       unit=self.UNIT, npt_azim=3600, 
+                                                       polarization_factor=self.polarization, 
+                                                       mask=mask0)
         #intrinsic_mask_unrolled = intrinsic_mask_unrolled.astype(bool) 
-        outlier_mask_2d = np.zeros_like(i2d)     # Create an array to hold outlier mask
-
-        i2d, q2d, chi2d = ai.integrate2d(img, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0)
+        
+        ## Create an array to hold outlier mask
+        outlier_mask_2d = np.zeros_like(i2d)     
         mask1 = np.array(i2d<1)*1
         
+        ## Apply percentile filter along radial direction (axis=0)
         for ii, dd in enumerate(i2d.T):
-            low_limit, high_limit = np.percentile(dd, (ll,ul))
+            low_limit, high_limit = np.percentile(dd, (self.ll, self.ul))
             outlier_mask_2d[:,ii] = np.any([dd<low_limit, dd>high_limit, intrinsic_mask_unrolled[:,ii]], axis=0)
-        mask=outlier_mask_2d + mask1    
-        outlier_mask_2d_masked =  ma.masked_array(i2d, mask=outlier_mask_2d + mask1)     
-        data = np.column_stack((q2d, ma.mean(outlier_mask_2d_masked,axis=0)))
-        #plt.plot(q2d, ma.mean(outlier_mask_2d_masked,axis=0))
-        # np.savetxt(directory + "/dark_sub/sum/" + str(a) + "_L" + str(ll)+ "_U" + str(ul) + "_percentile_masked_"+ str(UNIT) + ".dat", data) # Uncomment when the data is ready to be saved
+          
+        outlier_mask_2d_masked = ma.masked_array(i2d, mask=outlier_mask_2d + mask1)
+        
+        ## calculate mean values along radial direction (axis=0) to make i1d.shape is (self.binning, )
+        i1d = ma.mean(outlier_mask_2d_masked, axis=0)
+        
+        df = pd.DataFrame()
+        df['q'] = q1d
+        df['I'] = i1d
+        iq_fn = os.path.join(sum_dir, f'{self.file_name_prefix}_sum.iq')
+        md = ai.getPyFAI()
+        _md = {'detector': self.run.start['sp_detector'], 
+               'uid':self.full_uid, 
+               'time': self.run.start['time'], 
+               'readable_time': self.readable_time, 
+               'percentile_low_limit': self.ll, 
+               'percentile_up_limit': self.ul, 
+               }
+        md.update(_md)
 
-        data_fn = os.path.join(directory, fn_prefix+"_L"+str(ll)+"_U"+str(ul)+"_percentile_masked_"+str(UNIT)+".dat")
-        np.savetxt(data_fn, data) # Uncomment when the data is ready to be saved
+        iq_saver(iq_fn, df, md)
+        print(f'\n*** {os.path.basename(iq_fn)} saved!! ***\n')
 
-        print(f'\n*** {os.path.basename(data_fn)} saved!! ***\n')
-
-        return data, data_fn
+        return df, iq_fn
 
 
 
 '''
-
-from pdfstream.io import load_array
-from pdfstream.transformation.io import load_pdfconfig, write_pdfgetter
-from pdfstream.transformation.main import get_pdf
-from diffpy.pdfgetx import PDFConfig
-from diffpy.pdfgetx.pdfgetter import PDFConfigError
-import typing
-
-
-## Copy from https://github.com/NSLS2/xpd-profile-collection-ldrd20-31/blob/main/scripts/_get_pdf.py
-def transform_bkg(
-    cfg_file,
-    data_file: str,
-    output_dir: str = ".",
-    plot_setting: typing.Union[str, dict] = None,
-    test: bool = False,
-    gr_fn: str = '/home/xf28id2/Documents/test.gr', 
-    ) -> typing.Dict[str, str]:
-    
-    """Transform the data."""
-    if isinstance(cfg_file,str):
-        pdfconfig = load_pdfconfig(cfg_file)
-    else:
-        pdfconfig = cfg_file
-    
-    if type(data_file) is str:
-        chi = load_array(data_file)
-    elif type(data_file) is np.ndarray:
-        chi = data_file
-    
-    try:
-        pdfgetter = get_pdf(pdfconfig, chi, plot_setting=plot_setting)
-    except PDFConfigError:
-        pdfgetter = get_pdf(pdfconfig, chi, plot_setting='OFF')
-    
-    # filename = Path(data_file).stem
-    # dct = write_pdfgetter(output_dir, filename, pdfgetter)
-    
-    dct = write_pdfgetter(output_dir, gr_fn, pdfgetter)
-    if not test:
-        plt.show()
-    
-    return dct, pdfconfig
-
-
-## Add by CHLin on 2025/06/13 to turn sample_composition from dict to string
-def composition_maker(scan_comp):
-    com = ''
-    for i in scan_comp.keys():
-        com += f'{i} {scan_comp[i] }'
-
-    return com
-
-
-
-def get_gr(uid, iq_data, cfg_fn, bkg_fn, output_dir, gr_fn_prefix):
-    run = tiled_client[uid]
-    
-    ## run.start['sample_composition'] is a dict but pdfconfig takes a string for composition 
-    # scan_com = run.start['sample_composition']
-
-    pdfconfig = PDFConfig()def sum_everything(uid, stream_name, masks_path, masks_pos_flist, osetx = 27, osety = 27):
-    """ Assuming im2 offset by -osetx, -osety, and im3 offset by +osetx, +osety """
-    
-    run = tiled_client[uid]
-    my_im3 = getattr(run, stream_name[0]).read()['pilatus1_image'].to_numpy()[0][0]
-    my_im2 = getattr(run, stream_name[1]).read()['pilatus1_image'].to_numpy()[0][0]
-    my_im1 = getattr(run, stream_name[2]).read()['pilatus1_image'].to_numpy()[0][0]
-
-
-    # masks_pos_fn = ['Mask_pos1_ext_BS.npy', 'Mask_pos2_ext_BS.npy', 'Mask_pos3_ext_BS.npy']
-    use_mask_1= np.load(os.path.join(masks_path, masks_pos_flist[0])) # This is mask we are applying befor mergin images
-    use_mask_2 = np.load(os.path.join(masks_path, masks_pos_flist[1])) # This is mask we are applying befor mergin images
-    use_mask_3 = np.load(os.path.join(masks_path, masks_pos_flist[2])) # This is mask we are applying befor mergin images
-
-    my_imsum = np.ones((my_im1.shape[0]+int(2*osetx), my_im2.shape[1]+int(2*osety),3))*np.nan
-
-    my_imsum[osetx:-osetx,osety:-osety,0] = my_im1
-    my_imsum[osetx:-osetx,osety:-osety,0][use_mask_1==1] = np.nan
-
-    my_imsum[:-int(2*osetx),:-int(2*osety):,1] = my_im2
-    my_imsum[:-int(2*osetx),:-int(2*osety):,1][use_mask_2==1] = np.nan
-
-    my_imsum[int(2*osetx):,int(2*osety):,2] = my_im3
-    my_imsum[int(2*osetx):,int(2*osety):,2][use_mask_3==1] = np.nan
-    return np.nanmean(my_imsum,axis=2)
-
-
-def save_image_sum_T(uid, stream_name, sample_name, osetx = 27, osety = 27, 
-                     masks_pos_flist = ['Mask_pos1_ext_BS.npy', 'Mask_pos2_ext_BS.npy', 'Mask_pos3_ext_BS.npy'], 
-                     masks_path = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/config_base/pilatus_mask',  
-                     tiff_base_path = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/tiff_base',
-                     ):
-    
-    data_dir = os.path.join(tiff_base_path, sample_name, "dark_sub")
-    meta_dir = os.path.join(tiff_base_path, sample_name, "meta")
-    Meta = glob.glob(os.path.join(meta_dir, f'**{uid[:6]}.yaml'))
-
-    File_Name_Prefix = os.path.basename(Meta[0]).split('.')[0]
-    sum_dir = os.path.join(data_dir, 'sum')
-    os.makedirs(sum_dir, exist_ok=True)  # Create 'sum' directory if it doesn't exist
-        
-    try:
-        with open(os.path.join(meta_dir, Meta[0]), 'r') as f:
-            data = yaml.unsafe_load(f)
-            T = data.get('more_info')['cryostat_A']
-    except (KeyError, IndexError, TypeError):
-        T = 'None'
-    
-    full_imsum = sum_everything(uid, stream_name, masks_path, masks_pos_flist, osety=osety, osetx=osetx)
-    
-    # Save output file to the new 'sum' directory
-    sum_tiff_fn = f"{sum_dir}/{File_Name_Prefix}_Temperature_{T}_K_sum.tiff"
-    tifffile.imsave(sum_tiff_fn, full_imsum)
-    print(f'\n*** {os.path.basename(sum_tiff_fn)} saved!! ***\n')
-
-    saved_fn_prefix = os.path.basename(sum_tiff_fn).split('.')[0]
-
-    return full_imsum, sum_dir, saved_fn_prefix
-
-
-def pct_integration(img, fn_prefix, binning=5000, polarization=0.99, UNIT = "q_A^-1", ll=1, ul=99, 
-                    poni_fn = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/config_base/merged_PDF/merged.poni', 
-                    mask_fn = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/config_base/pilatus_mask/stitched_2.npy',
-                    directory = '/nsls2/data3/pdf/pdfhack/legacy/processed/xpdacq_data/user_data/tiff_base',
-                    ):
-    
-    ai = pyFAI.load(poni_fn)
-    mask0 = np.load(mask_fn)
-    
-    i2d, q2d, chi2d = ai.integrate2d(img, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0) # perform azimuthalintegration on one image to retain 2D information
-    intrinsic_mask_unrolled,_,_ = ai.integrate2d(mask0, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0)  #trasnform mask0 (base mask) to the same coordinate space and cast it as type bool
-    #intrinsic_mask_unrolled = intrinsic_mask_unrolled.astype(bool) 
-    outlier_mask_2d = np.zeros_like(i2d)     # Create an array to hold outlier mask
-
-    i2d, q2d, chi2d = ai.integrate2d(img, binning, unit=UNIT, npt_azim=3600, polarization_factor=polarization, mask=mask0)
-    mask1 = np.array(i2d<1)*1
-    
-    for ii, dd in enumerate(i2d.T):
-        low_limit, high_limit = np.percentile(dd, (ll,ul))
-        outlier_mask_2d[:,ii] = np.any([dd<low_limit, dd>high_limit, intrinsic_mask_unrolled[:,ii]], axis=0)
-    mask=outlier_mask_2d + mask1    
-    outlier_mask_2d_masked =  ma.masked_array(i2d, mask=outlier_mask_2d + mask1)     
-    data = np.column_stack((q2d, ma.mean(outlier_mask_2d_masked,axis=0)))
-    #plt.plot(q2d, ma.mean(outlier_mask_2d_masked,axis=0))
-    # np.savetxt(directory + "/dark_sub/sum/" + str(a) + "_L" + str(ll)+ "_U" + str(ul) + "_percentile_masked_"+ str(UNIT) + ".dat", data) # Uncomment when the data is ready to be saved
-
-    data_fn = os.path.join(directory, fn_prefix+"_L"+str(ll)+"_U"+str(ul)+"_percentile_masked_"+str(UNIT)+".dat")
-    np.savetxt(data_fn, data) # Uncomment when the data is ready to be saved
-
-    print(f'\n*** {os.path.basename(data_fn)} saved!! ***\n')
-
-    return data, data_fn
-
-
-
-
 
 from pdfstream.io import load_array
 from pdfstream.transformation.io import load_pdfconfig, write_pdfgetter
